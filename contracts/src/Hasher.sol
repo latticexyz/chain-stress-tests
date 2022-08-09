45249b728b29a0db55ddeb7c3d481bce90826610
bytes <bytes@lattice.xyz>
2022-08-09 16:10:41 -0400

    refactor(contracts): edit hasher

--- contracts/src/Hasher.sol
+++ contracts/src/Hasher.sol
@@ -2,14 +2,18 @@
 pragma solidity >=0.8.0;
 
 contract Hasher {
-    bytes32 public lastHash;
+    bytes32 private _lastHash;
 
-    // Hash lastHash nn times
+    function getHash() external view returns (bytes32) {
+        return _lastHash;
+    }
+
+    // Hash _lastHash nn times
     function hash(uint256 nn) public {
-        bytes32 currentHash = lastHash;
+        bytes32 currentHash = _lastHash;
         for (uint256 ii = 0; ii < nn; ii++) {
             currentHash = keccak256(abi.encode(currentHash));
         }
-        lastHash = currentHash;
+        _lastHash = currentHash;
     }
 }
