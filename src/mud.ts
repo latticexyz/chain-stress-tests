70d1643b394bec916a641d62d1d981f5f5f1abc9
bytes <bytes@lattice.xyz>
2022-08-23 16:38:42 +0200

    feat: clean coords

--- src/mud.ts
+++ src/mud.ts
@@ -21,6 +21,9 @@ const TYPE_COMPONENT_ADDRESS = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
 // Hard-coded gas limit
 const GAS_LIMIT = 250000;
 
+const BOX_POS = [128, 0, 4];
+const BOX_SIZE = [1, 1, 1];
+
 function random(to: number, from = 0): number {
   return Math.floor(Math.random() * (to - from + 1)) + from;
 }
@@ -40,7 +43,7 @@ const paramsFunc: ParamsFunc = async (callContext, testContext) => {
     args = [
       TYPE_COMPONENT_ADDRESS,
       callContext.walletIdx,
-      abi.encode(["uint32"], [12]),
+      abi.encode(["uint32"], [6]), // Stone
     ];
   } else {
     args = [
@@ -49,7 +52,11 @@ const paramsFunc: ParamsFunc = async (callContext, testContext) => {
       abi.encode(
         // x y z
         ["int32", "int32", "int32"],
-        [random(64, -32), random(64, -32), random(32, 0)]
+        [
+          random(BOX_POS[0], BOX_POS[0] + BOX_SIZE[0]),
+          random(BOX_POS[1], BOX_POS[1] + BOX_SIZE[1]),
+          random(BOX_POS[2], BOX_POS[2] + BOX_SIZE[2]),
+        ]
       ),
     ];
   }
